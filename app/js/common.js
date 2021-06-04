6f42e57456871f125dee7eb455e45e7c399d7156
Dmitriy-WhyMe <65383975+Dmitriy-WhyMe@users.noreply.github.com>
2021-06-04 15:11:24 +0300

    dop_mobile

--- app/js/common.js
+++ app/js/common.js
@@ -3,6 +3,21 @@
 		$(function() {
 			$('#main-menu').smartmenus();
 		});
+		$(window).scroll(function() {
+			var scrolled = $(window).scrollTop() + $(window).height();
+			var heightscreen = $(window).height();
+			var heightscreen2 = $(window).scrollTop();
+			if ( scrolled > heightscreen) {
+				$("#main-menu").css("display", "none");
+			}
 
+			if ( scrolled > 50) {
+				$("#main-menu-state").trigger( "click" );
+			}
+			console.log(heightscreen2);
+		});
+		document.querySelector(".main-menu-btn").onclick = function(){
+			$("#main-menu").css("display", "block");
+		};
 	});
 })(jQuery);
