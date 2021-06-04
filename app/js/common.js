53fd6be989f77508fcc0e102f7595b227db87388
Dmitriy-WhyMe <65383975+Dmitriy-WhyMe@users.noreply.github.com>
2021-06-04 20:58:19 +0300

    bag_menu

--- app/js/common.js
+++ app/js/common.js
@@ -5,19 +5,11 @@
 		});
 		$(window).scroll(function() {
 			var scrolled = $(window).scrollTop() + $(window).height();
-			var heightscreen = $(window).height();
-			var heightscreen2 = $(window).scrollTop();
-			if ( scrolled > heightscreen) {
-				$("#main-menu").css("display", "none");
-			}
-
 			if ( scrolled > 50) {
-				$("#main-menu-state").trigger( "click" );
+				if ( $('#main-menu-state').prop('checked') ) {
+					$("#clicks").trigger( "click" );
+				}
 			}
-			console.log(heightscreen2);
 		});
-		document.querySelector(".main-menu-btn").onclick = function(){
-			$("#main-menu").css("display", "block");
-		};
 	});
 })(jQuery);
